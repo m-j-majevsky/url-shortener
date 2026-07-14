32bf8822ae43f539aa43a3aa7581ee126cac6e38
Mike Nosov <roglick@gmail.com>
2026-07-14 23:30:29 +0300

    iter4 review comments resolution attempt

--- internal/base62/validator_test.go
+++ internal/base62/validator_test.go
@@ -8,50 +8,36 @@ import (
 
 func TestValidateBase62(t *testing.T) {
 	tests := []struct {
-		name string
-		s    string
-		want *base62.Base62Error
+		name   string
+		s      string
+		passed bool
 	}{
 		{
-			name: "Проверка на успешность валидации base62-строки",
-			s:    "AsD0h8Ds",
-			want: nil,
+			name:   "Проверка на успешность валидации base62-строки",
+			s:      "AsD0h8Ds",
+			passed: true,
 		},
 		{
-			name: "Проверка на невалидность пустой строки",
-			s:    "",
-			want: &base62.Base62Error{
-				Input:   "",
-				BadChar: 0,
-				Message: "пустая строка не является допустимым base62-токеном",
-			},
+			name:   "Проверка на невалидность пустой строки",
+			s:      "",
+			passed: false,
 		},
 		{
-			name: "Проверка на недопустимость не base62-символов ASCII",
-			s:    "a_String",
-			want: &base62.Base62Error{
-				Input:   "a_String",
-				BadChar: '_',
-				Message: "недопустимый base62-символ: '_'",
-			},
+			name:   "Проверка на недопустимость не base62-символов ASCII",
+			s:      "a_String",
+			passed: false,
 		},
 		{
-			name: "Проверка на недопустимость не base62-символов не из ASCII",
-			s:    "aΩString",
-			want: &base62.Base62Error{
-				Input:   "aΩString",
-				BadChar: 'Ω',
-				Message: "недопустимый символ (не ASCII): 'Ω'",
-			},
+			name:   "Проверка на недопустимость не base62-символов не из ASCII",
+			s:      "aΩString",
+			passed: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := base62.ValidateBase62(tt.s)
-			if (tt.want == nil && got != nil) ||
-				(tt.want != nil && got == nil) ||
-				(tt.want != nil && got.Message != tt.want.Message) {
-				t.Errorf("ValidateBase62() = %v, want %v", got, tt.want)
+			err := base62.ValidateBase62(tt.s)
+			if tt.passed != (err == nil) {
+				t.Errorf("ошибка валидации: %v", err)
 			}
 		})
 	}
