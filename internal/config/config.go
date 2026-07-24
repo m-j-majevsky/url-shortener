1b4d861f7fc809fba2a86fc3a08531b802b18711
Mike Nosov <roglick@gmail.com>
2026-07-24 14:08:30 +0300

    iter6 logger configuration and usage added

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,7 @@ import (
 )
 
 type ApplicationConfig struct {
+	LogLevel         string
 	ServerRunAddress string
 	TargetBaseURL    string
 	ServiceConfig    service.ShortenerConfig
@@ -19,6 +20,10 @@ func MakeApplicationConfig() ApplicationConfig {
 	appConfig := ApplicationConfig{ServiceConfig: svcConfig}
 
 	parseFlags(&appConfig)
+
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		appConfig.LogLevel = envLogLevel
+	}
 
 	if envServAddr := os.Getenv("SERVER_ADDRESS"); envServAddr != "" {
 		appConfig.ServerRunAddress = envServAddr
@@ -34,5 +39,6 @@ func MakeApplicationConfig() ApplicationConfig {
 func parseFlags(cfg *ApplicationConfig) {
 	flag.StringVar(&cfg.ServerRunAddress, "a", ":8080", "address and port to run server")
 	flag.StringVar(&cfg.TargetBaseURL, "b", "http://localhost:8080", "target URL base path")
+	flag.StringVar(&cfg.LogLevel, "l", "info", "log level")
 	flag.Parse()
 }
