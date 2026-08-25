53c42c24520af4c18e9e178269ccaf3c51acdea0
Mike Nosov <roglick@gmail.com>
2026-08-26 01:17:59 +0300

    iter15 starting commit with some correction of misprints in comments

--- migrations/0003_users_and_shorten_urls_to_users_ref.down.sql
+++ migrations/0003_users_and_shorten_urls_to_users_ref.down.sql
@@ -1,7 +1,8 @@
 DROP INDEX IF EXISTS idx_shorten_urls_user_id;
 
 -- Из-за ограничений процедур зачистки тестовых сценариев Практикума в GitHub Actions
--- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов
+-- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов процедура зачистки
+-- не задействует down-скрипты миграции
 --ALTER TABLE shorten_urls 
 --DROP CONSTRAINT IF EXISTS fk_shorten_urls_user_id;
 
--- migrations/0003_users_and_shorten_urls_to_users_ref.up.sql
+++ migrations/0003_users_and_shorten_urls_to_users_ref.up.sql
@@ -7,7 +7,8 @@ ALTER TABLE shorten_urls
 ADD COLUMN user_id uuid;
 
 -- Из-за ограничений процедур зачистки тестовых сценариев Практикума в GitHub Actions
--- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов
+-- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов процедура зачистки
+-- не задействует down-скрипты миграции
 --ALTER TABLE shorten_urls
 --ADD CONSTRAINT fk_shorten_urls_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
