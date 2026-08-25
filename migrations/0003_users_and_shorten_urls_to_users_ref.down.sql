a508fa3917f150c946e21925543f7c3a42b68420
Mike Nosov <roglick@gmail.com>
2026-08-26 01:11:46 +0300

    iter14 migration scripts updated for some external tests limitations

--- migrations/0003_users_and_shorten_urls_to_users_ref.down.sql
+++ migrations/0003_users_and_shorten_urls_to_users_ref.down.sql
@@ -1,7 +1,9 @@
 DROP INDEX IF EXISTS idx_shorten_urls_user_id;
 
-ALTER TABLE shorten_urls 
-DROP CONSTRAINT IF EXISTS fk_shorten_urls_user_id;
+-- Из-за ограничений процедур зачистки тестовых сценариев Практикума в GitHub Actions
+-- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов
+--ALTER TABLE shorten_urls 
+--DROP CONSTRAINT IF EXISTS fk_shorten_urls_user_id;
 
 ALTER TABLE shorten_urls 
 DROP COLUMN IF EXISTS user_id;
--- migrations/0003_users_and_shorten_urls_to_users_ref.up.sql
+++ migrations/0003_users_and_shorten_urls_to_users_ref.up.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
 ALTER TABLE shorten_urls 
 ADD COLUMN user_id uuid;
 
-ALTER TABLE shorten_urls
-ADD CONSTRAINT fk_shorten_urls_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+-- Из-за ограничений процедур зачистки тестовых сценариев Практикума в GitHub Actions
+-- придется пока отказаться от выстановки FK, т.к. при проверке ранних спринтов
+--ALTER TABLE shorten_urls
+--ADD CONSTRAINT fk_shorten_urls_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
 CREATE INDEX idx_shorten_urls_user_id ON shorten_urls(user_id);
